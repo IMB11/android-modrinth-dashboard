71379e6d90d677f4ea7fb47a611e993451d4c835
mineblock11 <93472213+mineblock11@users.noreply.github.com>
2023-04-13 17:44:48 +0100

    Initial Commit

---
+++ android/app/src/main/java/com/mineblock11/modrinthdash/MainActivity.java
@@ -0,0 +1,5 @@
+package com.mineblock11.modrinthdash;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
